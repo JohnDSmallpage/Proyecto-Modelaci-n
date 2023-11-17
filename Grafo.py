7987b59e7b723b8685c6604cfe38dde8200d0e77
GabrielSuarez <suarezgabrielr@gmail.com>
2023-11-16 21:38:39 -0400

    prueba final #1

--- Grafo.py
+++ Grafo.py
@@ -1,8 +1,12 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
 class Grafo:
     def __init__(self, num_nodos, pesos):
         self.num_nodos = num_nodos
         self.pesos = pesos
         self.matriz_adyacencia = self.generar_matriz()
+        self.grafo = self.crear_grafo()
 
     def generar_matriz(self):
         matriz = [[0] * self.num_nodos for _ in range(self.num_nodos)]
@@ -11,6 +15,116 @@ class Grafo:
             matriz[j][i] = peso  # Si el grafo es no dirigido
         return matriz
     
+    def dijkstra(self, punto_i, punto_y):
+        visitados = [False] * self.num_nodos
+        distancia = [float('inf')] * self.num_nodos
+        distancia[punto_i] = 0
+        camino = {nodo: [] for nodo in range(self.num_nodos)}
+        camino[punto_i].append(punto_i)
+        
+        while not all(visitados):
+            # Encuentra el nodo no visitado con la distancia mínima
+            min_distancia = float('inf')
+            min_nodo = -1
+            for nodo in range(self.num_nodos):
+                if not visitados[nodo] and distancia[nodo] < min_distancia:
+                    min_distancia = distancia[nodo]
+                    min_nodo = nodo
+
+            # Marca el nodo como visitado
+            visitados[min_nodo] = True
+            
+            # Actualiza las distancias de los nodos vecinos
+            for vecino in range(self.num_nodos):
+                if not visitados[vecino] and self.matriz_adyacencia[min_nodo][vecino] > 0:
+                    nueva_distancia = distancia[min_nodo] + self.matriz_adyacencia[min_nodo][vecino]
+                    if nueva_distancia < distancia[vecino]:
+                        distancia[vecino] = nueva_distancia
+                        camino[vecino] = camino[min_nodo] + [vecino]
+
+        print(f"Distancias más cortas desde el nodo {punto_i}:")
+        # for nodo, dist in enumerate(distancia):
+        print(f"Nodo {punto_y}: {distancia[punto_y]}, Camino: {camino[punto_y]}")
+        return camino[punto_y],distancia[punto_y]
+    
+    #Funcion que evalua por minuto donde se encuentra Javier y andreina, si nunca se encuentran define los caminos
+    #de lo contrario se vuelve a llamar a la funcion en main quitando una ruta
+    def tiempos(self,grafo_andreina, camino_javier, camino_andreina,distancia_javier,distancia_andreina):
+        tiempo_javier = {} #diccionario para minuto-camino para javier
+        tiempo_andreina = {}#diccionario para minuto-camino para andreina
+        if distancia_andreina > distancia_javier: #si la distancia de andreina es mayor
+            retraso = distancia_andreina-distancia_javier
+            print(f"Javier debe salir {retraso} minutos tarde")
+            tiempo = 0
+            for y, i in enumerate(camino_andreina):
+                if i != camino_andreina[-1]: #haya los caminos por los que pasa andreina cada minutos
+                    while grafo_andreina.matriz_adyacencia[i][camino_andreina[y+1]] > 0:
+                        grafo_andreina.matriz_adyacencia[i][camino_andreina[y+1]] =grafo_andreina.matriz_adyacencia[i][camino_andreina[y+1]] -1
+                        tiempo +=1
+                        tiempo_andreina[tiempo]=(i,camino_andreina[y+1])
+            tiempo = retraso
+            for y, i in enumerate(camino_javier): #halla los caminos por los que pasa javier empezando por los min de retraso
+                if i != camino_javier[-1]:
+                    while self.matriz_adyacencia[i][camino_javier[y+1]] > 0:
+                        self.matriz_adyacencia[i][camino_javier[y+1]] =self.matriz_adyacencia[i][camino_javier[y+1]] -1
+                        tiempo +=1
+                        tiempo_javier[tiempo]=(i,camino_javier[y+1])
+            for i in tiempo_javier:
+                if tiempo_javier[i] == tiempo_andreina[i] :
+                    print(f"En el minuto {i} estan caminando juntos en la ruta {tiempo_javier[i]}")
+                    return False,"javier",tiempo_javier[i]
+
+                elif i == distancia_javier+retraso:
+                    print("LLegaron sin ser vistos caminando juntos")
+                    return True,"",tiempo_javier[i]
+                
+        else:
+            #igual que arriba pero para cuando la distancia de javier es mayor
+            retraso = distancia_javier-distancia_andreina
+            print(f"Javier debe salir {retraso} minutos tarde")
+            tiempo = retraso
+            for y, i in enumerate(camino_andreina):
+                if i != camino_andreina[-1]:
+                    while grafo_andreina.matriz_adyacencia[i][camino_andreina[y+1]] > 0:
+                        grafo_andreina.matriz_adyacencia[i][camino_andreina[y+1]] =grafo_andreina.matriz_adyacencia[i][camino_andreina[y+1]] -1
+                        tiempo +=1
+                        tiempo_andreina[tiempo]=(i,camino_andreina[y+1])
+            tiempo = 0
+            for y, i in enumerate(camino_javier):
+                if i != camino_javier[-1]:
+                    while self.matriz_adyacencia[i][camino_javier[y+1]] > 0:
+                        self.matriz_adyacencia[i][camino_javier[y+1]] =self.matriz_adyacencia[i][camino_javier[y+1]] -1
+                        tiempo +=1
+                        tiempo_javier[tiempo]=(i,camino_javier[y+1])
+            for i in tiempo_andreina:
+                if tiempo_andreina[i] == tiempo_javier[i]:
+                    print(f"En el minuto {i} estan caminando juntos en la ruta {tiempo_andreina[i]}")
+                    return False,"andreina",tiempo_andreina[i]
+                elif i == distancia_andreina+retraso:
+                    print("LLegaron sin ser vistos caminando juntos")
+                    return True,"",tiempo_andreina[i]
+
+    def crear_grafo(self):
+        G = nx.Graph()
+        for (i, j), peso in self.pesos.items():
+            G.add_edge(i, j, weight=peso)
+        return G
+    
     def imprimir_matriz(self):
         for fila in self.matriz_adyacencia:
             print(fila)
+
+
+    def imprimir_grafico(self):
+        G = nx.Graph()
+        for (i, j), peso in self.pesos.items():
+            G.add_edge(i, j, weight=peso)
+        pos = nx.spring_layout(G)
+        labels = nx.get_edge_attributes(G, 'weight')
+        nx.draw(G, pos, with_labels=True, node_size=700, node_color='lightblue', font_weight='bold', font_size=10)
+        nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
+        plt.show()
+
+    #Codigo de pruebas!!
+    # def camino_optimo(self, punto_i, punto_y):
+    #     return nx.dijkstra_path(self.grafo, punto_i, punto_y)
--- main.py
+++ main.py
@@ -4,13 +4,35 @@ def main():
 
     # Ejemplo de uso
     pesos_javier = {
-        (0, 6): 5, (0, 1): 5, (1, 2): 5, (1, 7): 7, (3, 4): 5, (3, 9): 7, (4, 5): 5, (4, 10): 5, (5, 11): 5, (6, 12): 5, (6,7):5,(7,8):5,(7,13):7,(8,9):5,(8,14):7,(9,10):5,(9,15):7,(10,16):5,(11,17):5,(12,13):5,(12,18):5,(13,14):5,(13,19):7,(14,15):5,(14,20):7,(15,16):5,(15,21):7,(16,17):5,(16,22):5,(17,23):5,(18,19):5,(18,24):5,(19,20):5,(19,25):7,(20,21):5,(20,26):7,(21,22):5,(21,27):7,(22,23):5,(22,28):5,(23,29):5,(24,25):10,(24,30):5,(25,26):10,(25,31):7,(26,27):10,(26,32):7,(27,28):10,(27,33):7,(28,29):10,(28,34):5,(29,35):5,(30,31):5,(31,32):5,(32,33):5,(33,34):5,(34,35):5}
+        (0, 6): 5, (0, 1): 5, (1, 2): 5, (1, 7): 7, (3, 4): 5, (3, 9): 7, (4, 5): 5, (4, 10): 5, (5, 11): 5, (6, 12): 5, (6,7):5,(7,8):5,(7,13):7,(8,9):5,(8,14):7,(9,10):5,(9,15):7,(10,11):5,(10,16):5,(11,17):5,(12,13):5,(12,18):5,(13,14):5,(13,19):7,(14,15):5,(14,20):7,(15,16):5,(15,21):7,(16,17):5,(16,22):5,(17,23):5,(18,19):5,(18,24):5,(19,20):5,(19,25):7,(20,21):5,(20,26):7,(21,22):5,(21,27):7,(22,23):5,(22,28):5,(23,29):5,(24,25):10,(24,30):5,(25,26):10,(25,31):7,(26,27):10,(26,32):7,(27,28):10,(27,33):7,(28,29):10,(28,34):5,(29,35):5,(30,31):5,(31,32):5,(32,33):5,(33,34):5,(34,35):5}
 
     pesos_andreina = {
         (0, 6): 7, (0, 1): 7, (1, 2): 7, (1, 7): 9, (3, 4): 7, (3, 9): 9, (4, 5): 7, (4, 10): 7, (5, 11): 7, (6, 12): 7, (6,7):7,(7,8):7,(7,13):9,(8,9):7,(8,14):9,(9,10):7,(9,15):9,(10,16):7,(11,17):7,(12,13):7,(12,18):7,(13,14):7,(13,19):9,(14,15):7,(14,20):9,(15,16):7,(15,21):9,(16,17):7,(16,22):7,(17,23):7,(18,19):7,(18,24):7,(19,20):7,(19,25):9,(20,21):7,(20,26):9,(21,22):7,(21,27):9,(22,23):7,(22,28):7,(23,29):7,(24,25):12,(24,30):7,(25,26):12,(25,31):9,(26,27):12,(26,32):9,(27,28):12,(27,33):9,(28,29):12,(28,34):7,(29,35):7,(30,31):7,(31,32):7,(32,33):7,(33,34):7,(34,35):7}
     
     grafo_javier = Grafo(36, pesos_javier)
-    grafo_javier.imprimir_matriz() 
+    grafo_andreina = Grafo(36, pesos_andreina)
+    # grafo_javier.imprimir_matriz() 
+    # grafo_javier.imprimir_grafico()
+    Casa_javier = 7
+    Final =31
+    Casa_andreina = 20
+    camino_javier,distancia_javier = grafo_javier.dijkstra(Casa_javier,Final)
+    camino_andreina,distancia_andreina = grafo_andreina.dijkstra(Casa_andreina,Final)
+    encontrado = False
+    while not encontrado: #evalua que no se haya encontrado unas rutas donde no se encuentran
+        #regresa si encontro camino, la persona con la distancia mas corta y el camino donde se encuentran
+        encontrado,nombre,camino = grafo_javier.tiempos(grafo_andreina,camino_javier,camino_andreina,distancia_javier,distancia_andreina)
+        # print(encontrado,nombre,camino)
+        if nombre == "andreina": #elimina el camino donde se encuentran
+            pesos_andreina[camino] = 0 
+        if nombre == "javier":
+            pesos_javier[camino] = 0 
+        if encontrado == False: #si no se encontro camino vuelve a inicializar las variables para la siguiente evaluacion
+            grafo_javier = Grafo(36, pesos_javier)
+            grafo_andreina = Grafo(36, pesos_andreina)
+            camino_javier,distancia_javier = grafo_javier.dijkstra(Casa_javier,Final)
+            camino_andreina,distancia_andreina = grafo_andreina.dijkstra(Casa_andreina,Final)
+
 
 main()
 
